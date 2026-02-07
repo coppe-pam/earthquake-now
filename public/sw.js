846d194a731d3bcbf85eb9c97f6cfbf93b14c7c7
こっぺぱん <kitsuneko.info@gmail.com>
2026-02-07 21:00:23 +0900

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,12 @@
-
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.open("eq-cache").then(cache =>
+      cache.match(e.request).then(r =>
+        r || fetch(e.request).then(res => {
+          cache.put(e.request, res.clone());
+          return res;
+        })
+      )
+    )
+  );
+});
